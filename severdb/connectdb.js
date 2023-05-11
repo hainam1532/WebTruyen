1f11ea76151f60a272c21393eb91ba0c97f4d456
hainam1532 <nnam83395@gmail.com>
2023-05-11 16:18:27 +0700

    upload source

---
+++ severdb/connectdb.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'WebDB'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Lỗi kết nối database: ' + err.stack);
+        return;
+    }
+
+    console.log('Đã kết nối với cơ sở dữ liệu dưới dạng id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM LOGIN', (err, rows, fields) => {
+    if (err) {
+        console.error('Lỗi khi thực hiện truy vấn: ' + err.stack);
+        return;
+    }
+
+    console.log('Kết quả truy vấn: ', rows);
+});
+
+connection.end();
