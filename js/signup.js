aa623ddc894c4aa5579756f9a7e8e61c80833c5b
RubeyOX <133131838+RubeyOX@users.noreply.github.com>
2023-05-12 18:31:13 +0700

    xét có nhập hay ko - hiện tên người dùng sau khi đăng ký -> chuyển sang trang chính

--- js/signup.js
+++ js/signup.js
@@ -1,14 +1,28 @@
-const sign_up_email=document.querySelector('sign_up_email')
-const sign_up_name=document.querySelector('sign_up_name')
-const sign_up_passowrd=document.querySelector('sign_up_password')
-const sign_up_btn=document.querySelector('site-btn')
-
-const sign_up=document.querySelector('sign_up')
-if(a){
-
-}else{
-    return dangky()
-}
+window.addEventListener('load',()=>{
+    const sign_up_email=document.querySelector('#sign_up_email')
+    const sign_up_name=document.querySelector('#sign_up_name')
+    const sign_up_passowrd=document.querySelector('#sign_up_password')
+    const sign_up_btn=document.querySelector('.site-btn')
+    const sign_up=document.querySelector('#sign_up')
+    const header_right=document.querySelector('.header__right')
+    sign_up.addEventListener('submit',()=>{
+        const email=sign_up_email.value
+        const name=sign_up_name.value
+        const passowrd=sign_up_passowrd.value
+        sign_up_btn.addEventListener('click',()=>{
+            if(email && name && passowrd){
+                const profile_name=document.createElement('p')
+                profile_name.classList.add('profile_name')
+                profile_name.innerHTML=name
+                header_right.appendChild(profile_name)
+                sign_up_email.value=''
+                sign_up_name.value=''
+                sign_up_passowrd.value=''
+                return dangky()
+            }
+        })
+    })
+})
 function dangky(){
-    window.open("index.html","blank");
+    window.open('index.html')
 }
