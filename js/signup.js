23ffa17b6f5eca855f62d55fe637f7bac7a9e851
RubeyOX <133131838+RubeyOX@users.noreply.github.com>
2023-05-13 19:08:30 +0700

    nope

--- js/signup.js
+++ js/signup.js
@@ -1,28 +1,34 @@
-window.addEventListener('load',()=>{
-    const sign_up_email=document.querySelector('#sign_up_email')
-    const sign_up_name=document.querySelector('#sign_up_name')
-    const sign_up_passowrd=document.querySelector('#sign_up_password')
-    const sign_up_btn=document.querySelector('.site-btn')
-    const sign_up=document.querySelector('#sign_up')
-    const header_right=document.querySelector('.header__right')
-    sign_up.addEventListener('submit',()=>{
-        const email=sign_up_email.value
-        const name=sign_up_name.value
-        const passowrd=sign_up_passowrd.value
-        sign_up_btn.addEventListener('click',()=>{
-            if(email && name && passowrd){
-                const profile_name=document.createElement('p')
-                profile_name.classList.add('profile_name')
-                profile_name.innerHTML=name
-                header_right.appendChild(profile_name)
-                sign_up_email.value=''
-                sign_up_name.value=''
-                sign_up_passowrd.value=''
-                return dangky()
-            }
-        })
+window.addEventListener('load', (e) => {
+    e.preventDefault();
+    const sign_up_email = document.querySelector('#sign_up_email')
+    const sign_up_name = document.querySelector('#sign_up_name')
+    const sign_up_password = document.querySelector('#sign_up_password')
+    const sign_up_btn = document.querySelector('.site-btn')
+    const sign_up = document.querySelector('#sign_up')
+    const header_right = document.querySelector('.header__right')``
+  
+    sign_up.addEventListener('submit', (e) => {
+      e.preventDefault(); // ngăn chặn submit form
+      const email = sign_up_email.value
+      const name = sign_up_name.value
+      const password = sign_up_password.value
+      if (email && name && password) {
+        const profile_name = document.createElement('p')
+        profile_name.classList.add('profile_name')
+        profile_name.innerHTML = name
+        header_right.appendChild(profile_name)
+        sign_up_email.value = ''
+        sign_up_name.value = ''
+        sign_up_password.value = ''
+      }
     })
-})
-function dangky(){
+  
+    sign_up_btn.addEventListener('click', () => {
+      dangky()
+    })
+  })
+  
+  function dangky() {
     window.open('index.html')
-}
+  }
+  
